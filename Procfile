41da41f672b93823c22dce5894d92783061a2789
Aditya Narayan Daw <aditya.n.daw@gmail.com>
2021-11-24 17:52:44 +0530

    Before pushing to Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=8000 price.wsgi:application
+web: waitress-serve --port=$PORT price.wsgi:application
--- price/settings.py
+++ price/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d2pe266on--o7ov34v2m9!lx=ht48rk7=w3$jz9rb$#+dgy(e9'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','stkprice.herokuapp.com']
 
 
 # Application definition
